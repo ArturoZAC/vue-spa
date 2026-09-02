547e3898e9a74e81cc0e6b9833c879cec24333bf
ArturoZAC <arturoyz2105@gmail.com>
2026-09-01 23:24:06 -0500

    feat: restructure authentication flow with login and registration pages

--- src/modules/auth/pages/RegisterPage.vue
+++ src/modules/auth/pages/RegisterPage.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- Right: Login Form -->
   <div class="lg:p-36 md:p-52 sm:20 p-8 w-full lg:w-1/2">
-    <h1 class="text-2xl font-semibold mb-4">Login</h1>
+    <h1 class="text-2xl font-semibold mb-4">Register</h1>
     <form action="#" method="POST">
       <!-- Username Input -->
       <div class="mb-4">
@@ -10,6 +10,17 @@
           type="text"
           id="username"
           name="username"
+          class="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
+          autocomplete="off"
+        />
+      </div>
+      <!-- Email Input -->
+      <div class="mb-4">
+        <label for="email" class="block text-gray-600">Email</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
           class="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
           autocomplete="off"
         />
@@ -44,7 +55,7 @@
     </form>
     <!-- Sign up  Link -->
     <div class="mt-6 text-blue-500 text-center">
-      <RouterLink to="/auth/register" class="hover:underline">Sign up Here</RouterLink>
+      <a href="#" class="hover:underline">Sign up Here</a>
     </div>
   </div>
 </template>
--- src/router/app.router.ts
+++ src/router/app.router.ts
@@ -35,7 +35,20 @@ export const appRouter = createRouter({
     {
       path: '/auth',
       name: 'auth',
-      component: () => import('../modules/auth/pages/LoginPage.vue'),
+      component: () => import('../modules/auth/layouts/AuthLayout.vue'),
+      redirect: '/auth/login',
+      children: [
+        {
+          path: 'login',
+          name: 'login',
+          component: () => import('../modules/auth/pages/LoginPage.vue'),
+        },
+        {
+          path: 'register',
+          name: 'register',
+          component: () => import('../modules/auth/pages/RegisterPage.vue'),
+        },
+      ],
     },
     {
       path: '/:pathMatch(.*)*',
